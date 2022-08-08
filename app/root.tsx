311e0af2c0856e0259310880083533f1029912cd
andrew <andrew@IDExpress1s-MacBook-Pro.local>
2022-08-08 12:34:24 +0700

    WIP jokes app

---
+++ app/root.tsx
@@ -0,0 +1,56 @@
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import globalStylesUrl from './styles/global.css';
+import globalMediumStylesUrl from './styles/global-medium.css';
+import globalLargeStylesUrl from './styles/global-large.css';
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'stylesheet',
+      href: globalStylesUrl
+    },
+    {
+      rel: 'stylesheet',
+      href: globalMediumStylesUrl,
+      media: "print, (min-width: 640px)",
+    },
+    {
+      rel: "stylesheet",
+      href: globalLargeStylesUrl,
+      media: "screen and (min-width: 1024px)",
+    }
+  ];
+}
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+        <title>Remix: So great, it's funny!</title>
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
